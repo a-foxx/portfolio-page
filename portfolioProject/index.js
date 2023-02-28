e126d836aa924de593f234d976e122889d5d097b
a-foxx <100381864+a-foxx@users.noreply.github.com>
2023-02-28 22:45:47 +0000

    Add files via upload

---
+++ portfolioProject/index.js
@@ -0,0 +1,6 @@
+{/* <script>
+// variable for id here
+element.addEventListener('onmouseover', mouseoverDiv)
+const mouseoverDiv = function mouseOver() {
+document.getElementById('bodywrapper').onmouseover = background-color: white; font-size: 110% });
+</script> */}
