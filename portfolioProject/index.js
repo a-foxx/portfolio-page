ebfbdee9324ea720ed9ded6ff37068fb648c6d40
a-foxx <100381864+a-foxx@users.noreply.github.com>
2023-03-01 14:44:00 +0000

    Add files via upload
    responsive + reactivity

--- portfolioProject/index.js
+++ portfolioProject/index.js
@@ -1,6 +1,26 @@
-{/* <script>
-// variable for id here
-element.addEventListener('onmouseover', mouseoverDiv)
-const mouseoverDiv = function mouseOver() {
-document.getElementById('bodywrapper').onmouseover = background-color: white; font-size: 110% });
-</script> */}
+
+//read more text
+
+const showMore = () => {
+    const dots = document.getElementById("dots");
+    const text = document.getElementById("more");
+    const button = document.getElementById("showTextBtn");
+
+    if (dots.style.display === "none") {
+        dots.style.display = 'inline';
+        button.innerHTML = 'Read More';
+        text.style.display = 'none';
+    } else {
+        dots.style.display = 'none';
+        button.innerHTML = 'Read Less';
+        text.style.display = 'inline';
+    }
+
+}
+
+//dark mode
+
+const darkMode = () => {
+    const element = document.body;
+    element.classList.toggle('dark-mode')
+}
